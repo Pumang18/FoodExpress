3566998108a7d1344e83032a47185830a583ed41
Pumang Kirange <kirangespumang03@gmail.com>
2025-07-03 00:02:08 +0530

    Update JwtUtils.java

--- Backend/src/main/java/com/capgemini/food_express/security/JwtUtils.java
+++ Backend/src/main/java/com/capgemini/food_express/security/JwtUtils.java
@@ -16,7 +16,7 @@ import java.util.function.Function;
 @Component
 public class JwtUtils {
 
-	public static final String SECRET = "357638792F423F4428472B4B6250655368566D597133743677397A2443264629";
+	public static final String SECRET = "";
 
 	public String extractUsername(String token) {
 		return extractClaim(token, Claims::getSubject);
